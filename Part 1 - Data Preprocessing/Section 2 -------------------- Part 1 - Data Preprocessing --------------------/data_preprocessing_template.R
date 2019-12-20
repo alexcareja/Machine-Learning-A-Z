1104a99ba9c78a1414cb684270b7c4e2b7a2fa2c
Alex Careja <44427360+alexcareja@users.noreply.github.com>
2019-12-20 20:00:44 +0200

    Add files via upload

---
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.R
@@ -0,0 +1,16 @@
+# Data Preprocessing Template
+
+# Importing the dataset
+dataset = read.csv("Data.csv")
+# dataset = dataset[, 2:3]
+
+# Splitting the dataset into the Training set and Test set
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# # Feature scaling
+# training_set[, 2:3] = scale(training_set[, 2:3])
+# test_set[, 2:3] = scale(test_set[, 2:3])
